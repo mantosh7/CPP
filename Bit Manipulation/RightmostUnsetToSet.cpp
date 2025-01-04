03cbc99df08b9b1f9d9d5e1f1eadf7d0fc98fec6
mantosh7 <mahiraval000@gmail.com>
2025-01-05 03:19:45 +0530

    Repplace rightmost unset(clear) with set

---
+++ Bit Manipulation/RightmostUnsetToSet.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int num = 6;
+    int mask = 1;
+    int c = num | mask;
+    while (c == num)
+    {
+        mask = mask << 1;
+        c = num | mask;
+    }
+    cout << c;
+    return 0;
+}
