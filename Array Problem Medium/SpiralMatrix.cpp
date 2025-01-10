b633fa6a9c1dc35e01a76dd323c91d8c7a9c8aaf
mantosh7 <mahiraval000@gmail.com>
2025-01-10 23:58:31 +0530

    making some changes

--- Array Problem Medium/SpiralMatrix.cpp
+++ Array Problem Medium/SpiralMatrix.cpp
@@ -2,9 +2,36 @@
 using namespace std;
 int main()
 {
-    int arr[3][4] = {1,2,3,4,5,6,7,8,9,10,11,12};
-    // int arr[3][1] = {7,9,6};
-    int minRow=0,maxRow=2,minCol=0,maxCol=3;
+    // taking inputs rows & columns
+    int n,m;
+    cout<<"Enter no.of rows:";
+    cin>>n;
+    cout<<"Enter no.of columns:";
+    cin>>m;
+    
+    int arr[n][m];
+    // input elements
+    cout<<"Enter elements:"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+    // printing array
+    cout<<"Original Matrix:"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    cout<<"Spiral matrix:"<<endl;
+    int minRow=0,maxRow=n-1,minCol=0,maxCol=m-1;
     while(minRow<=maxRow && minCol<=maxCol)
     {
         if(minRow<=maxRow && minCol<=maxCol)
@@ -43,5 +70,16 @@ int main()
             minCol++;
         }    
     }
+
+    // // printing spiral matrix
+    // cout<<"Spiral matrix:"<<endl;
+    // for(int i=0;i<n;i++)
+    // {
+    //     for(int j=0;j<m;j++)
+    //     {
+    //         cout<<arr[i][j]<<" ";
+    //     }
+    //     cout<<endl;
+    // }
     return 0;
 }
