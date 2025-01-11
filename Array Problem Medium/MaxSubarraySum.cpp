de0334cbfa84c8643348a9c39545bfb84d9884be
mantosh7 <mahiraval000@gmail.com>
2025-01-11 18:45:31 +0530

    making some changes

--- Array Problem Medium/MaxSubarraySum.cpp
+++ Array Problem Medium/MaxSubarraySum.cpp
@@ -1,26 +1,48 @@
+/*  PROBLEM STATEMENT
+Given an integer array nums, find the 
+subarray with the largest sum, and return its sum.
+
+Example 1:
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+*/
+
+
 #include<bits/stdc++.h>
 using namespace std;
-
 int main()
 {
-    vector<int> nums{-2,1,-3,4,-1,2,1,-5,4};
-    // vector<int>nums{1};
+    vector<int>nums{-2,1,-3,4,-1,2,1,-5,4};
     // vector<int>nums{5,4,-1,7,8};
+    // vector<int>nums{1};
     int n = nums.size();
 
-    int maxi = nums[0];
+    // Optimal Solution
     int sum = 0;
-
+    int maxSum = INT_MIN;
+    int startIdx = 0, endIdx = -1;
+    
     for(int i=0;i<n;i++)
     {
-        sum += nums[i];
-        if(sum > maxi) maxi = sum;
-        if(sum < 0) sum=0;
+        sum = sum + nums[i];
+        if(sum>maxSum)
+        {
+            maxSum = sum;
+            endIdx = i;
+        }
+        if(sum<0)
+        {
+            sum = 0;
+            startIdx = i+1;
+        }
     }
-
-    cout<<maxi;
-    return 0;
-
+    cout<<"subarray with maximum sum: "<<maxSum<<endl;
+    cout<<"Subarray: ";
+    for(int i=startIdx;i<=endIdx;i++)
+    {
+        cout<<nums[i]<<",";
+    }
 
     // Better Approach
     // int maxi = INT_MIN;
