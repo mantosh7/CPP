18128e9ce1128d7a6537980702165e83f76d2f1d
mantosh7 <mahiraval000@gmail.com>
2025-01-18 02:41:39 +0530

    organising lines of problem statement

--- Array Problem Medium/ArrayLeaders.cpp
+++ Array Problem Medium/ArrayLeaders.cpp
@@ -1,5 +1,7 @@
 /*
-PROBLEM STATEMENT : You are given an array arr of positive integers. Your task is to find all the leaders in the array. An element is considered a leader if it is greater than or equal to all elements to its right. The rightmost element is always a leader.
+PROBLEM STATEMENT : You are given an array arr of positive integers. Your task is to find
+all the leaders in the array. An element is considered a leader if it is greater than or
+equal to all elements to its right. The rightmost element is always a leader.
 
 Input: arr = [16, 17, 4, 3, 5, 2]
 Output: [17, 5, 2]
