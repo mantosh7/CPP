a1c7e37c611e5b2f0cfec453b92ba051a5fdb66b
mantosh7 <mahiraval000@gmail.com>
2025-01-21 23:22:53 +0530

    Adding problem statement

--- Array Problems Easy/missingNumber.cpp
+++ Array Problems Easy/missingNumber.cpp
@@ -1,9 +1,19 @@
+/*
+PROBLEM STATEMENT: Given an array nums containing n distinct numbers in the range [0, n], 
+return the only number in the range that is missing from the array.
+
+Example 1:
+Input: nums = [3,0,1]
+Output: 2
+*/
+
+
 #include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    // int nums[] = {9, 6, 4, 2, 3, 5, 7, 0, 1};
-    int nums[] = {3,0,1};
+    int nums[] = {9, 6, 4, 2, 3, 5, 7, 0, 1};
+    // int nums[] = {3,0,1};
     int size = sizeof(nums) / sizeof(nums[0]);
     // Optimal Code
     int XOR1 = 0;
