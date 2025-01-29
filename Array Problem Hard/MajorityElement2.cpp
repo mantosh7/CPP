4ddd99237e33812acd91037ce42b8825f763cc7d
mantosh7 <mahiraval000@gmail.com>
2025-01-30 01:36:34 +0530

    Adding Problem Statement

--- Array Problem Hard/MajorityElement2.cpp
+++ Array Problem Hard/MajorityElement2.cpp
@@ -1,3 +1,20 @@
+/*
+229. Majority Element II
+
+PROBLEM STATEMENT: Given an integer array of size n, find all elements that appear 
+more than ⌊ n/3 ⌋ times.
+
+Example 1:
+Input: nums = [3,2,3]
+Output: [3]
+
+Example 2:
+Input: nums = [1,2]
+Output: [1,2]
+*/
+
+
+
 #include<bits/stdc++.h>
 using namespace std;
 int main()
