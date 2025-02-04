8807530bca9af0da95d30f5afb08c4ab146a3e77
mantosh7 <mahiraval000@gmail.com>
2025-02-05 03:01:42 +0530

    Upper Bound Concept

--- BINARY SEARCH/Fundamental/UpperBound.cpp
+++ BINARY SEARCH/Fundamental/UpperBound.cpp
@@ -15,26 +15,30 @@ Explanation: Index 1 is the smallest index such that arr[1] >= x.
 #include<bits/stdc++.h>
 using namespace std;
 
-int lowerBound(vector<int>& nums, int x)
+int upperBound(vector<int>& nums, int x)
 {
-    int lbIdx = -1;
-    int low = 0, high = nums.size()-1 ;
-    while(low<=high)
+    int n = nums.size();
+    int ans = n ;
+    int low = 0, high = n-1 ;
+    while(low <= high)
     {
-        int mid = low + (high - low)/2 ;
-        
-        if(nums[mid] <= x) low = mid + 1;
-        else high = mid - 1;
+        int mid = low + (high - low) / 2 ;
+        if(nums[mid] > x)
+        {
+            ans = mid ;
+            high = mid- 1 ;
+        }
+        else low = mid + 1 ;
     }
-    return low;
+    return ans;
 }
 
 int main()
 {
-    // vector<int> nums = {1,2,2,3};
-    // int x = 2;
-    vector<int> nums = {3,5,8,15,19};
-    int x = 9;
+    vector<int> nums = {1,2,2,3};
+    int x = 2;
+    // vector<int> nums = {3,5,8,15,19};
+    // int x = 9;
 
-    cout<<lowerBound(nums,x);
+    cout<<upperBound(nums,x);
 }
