f775b5161898e873aa797dd5ee805ccceadc9f73
mantosh7 <mahiraval000@gmail.com>
2025-09-12 20:26:33 +0530

    No. of LIS

--- DP/NoOfLIS.cpp
+++ DP/NoOfLIS.cpp
@@ -1,3 +1,17 @@
+/*
+673. Number of Longest Increasing Subsequenc
+Problem Statement:
+Given an integer array nums, return the number of longest increasing subsequences.
+
+Notice that the sequence has to be strictly increasing.
+
+Example:
+Input: nums = [1,3,5,4,7]
+Output: 2
+Explanation: The two longest increasing subsequences are [1, 3, 4, 7] and [1, 3, 5, 7].
+
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 int main()
