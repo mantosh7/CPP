2142340f53ce6aba189d4da7723617fd25a7f469
mantosh7 <mahiraval000@gmail.com>
2025-09-12 20:24:48 +0530

    No. Of LIS

---
+++ DP/NoOfLIS.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int> nums = {1,3,5,4,7};
+    // vector<int> nums = {2, 2, 2, 2, 2};
+    int n = nums.size();
+    vector<int> dp(n, 1);
+    vector<int> numOfLongest(n, 1);
+
+    int maxi = 1;
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (nums[j] < nums[i])
+            {
+                if (dp[j] + 1 > dp[i])
+                {
+                    dp[i] = dp[j] + 1;
+                    numOfLongest[i] = numOfLongest[j];
+                }
+                else if (dp[j] + 1 == dp[i])
+                {
+                    numOfLongest[i] += numOfLongest[j];
+                }
+            }
+        }
+        maxi = max(maxi, dp[i]);
+    }
+
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (dp[i] == maxi)
+            ans += numOfLongest[i];
+    }
+    cout<< "The no. of LIS : " << ans;
+}
